a9a53200d2aec48424f08691eb5bbe79ae667434
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-08 00:33:22 +0530

    Add logical operators program

---
+++ CHAPTER-1/logicalOperator.py
@@ -0,0 +1,13 @@
+# Logical Operators in Python
+
+first_number = 10
+second_number = 5
+
+# AND operator
+print("AND:", first_number > 5 and second_number < 10)
+
+# OR operator
+print("OR:", first_number > 20 or second_number < 10)
+
+# NOT operator
+print("NOT:", not first_number < 5)
