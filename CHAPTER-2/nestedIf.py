2abae038f662c2575c9f8532f84f9a6ac31daa23
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-09 21:00:34 +0530

    Add nested if program

---
+++ CHAPTER-2/nestedIf.py
@@ -0,0 +1,8 @@
+age = 34 
+
+if (age >= 18):
+    print("You are eligible to vote.")
+    if (age >= 65):
+        print("You are eligible for senior citizen benefits.")
+    else:
+        print("You are not eligible for senior citizen benefits.")
