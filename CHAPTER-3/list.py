0c01d458c95313a9869075e30914fa376ac01a87
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-10 23:42:57 +0530

    Add Python list program

---
+++ CHAPTER-3/list.py
@@ -0,0 +1,9 @@
+marks = [90.4, 85.2, 78.5, 92.3, 88.0]
+student = ["Subho", 93, "Dalhi"]
+# print("Marks:", marks)
+# print("Marks:", type(marks))
+# print("Marks:", marks[2])
+print("Student:", student)
+
+student[1] = 95
+print("Updated Student:", student)
