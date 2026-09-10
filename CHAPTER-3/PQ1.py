8930b07f657a5f35a363af34e06d725fe1de10bd
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-10 23:44:44 +0530

    Add practice question 1

---
+++ CHAPTER-3/PQ1.py
@@ -0,0 +1,10 @@
+movies = []
+
+mov1 = input("Enter the name of the first movie: ")
+mov2 = input("Enter the name of the second movie: ")
+mov3 = input("Enter the name of the third movie: ")
+
+movies.append(mov1)
+movies.append(mov2) 
+movies.append(mov3)
+print("The list of movies is:", movies)
