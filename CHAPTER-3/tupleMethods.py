1e498898c4c1bd621e9c5018f491a38ebedcfe73
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-10 23:46:52 +0530

    Add tuple methods program

---
+++ CHAPTER-3/tupleMethods.py
@@ -0,0 +1,8 @@
+tuple = (4, 5, 5, 5, 5, 1, 3, 2)
+
+index = tuple.index(3)  # Returns the index of the first occurrence of 3 in the tuple
+print("Index of 3 in the tuple:", index)
+
+
+count = tuple.count(5)  # Returns the number of occurrences of 5 in the tuple
+print("Number of occurrences of 5 in the tuple:", count)
