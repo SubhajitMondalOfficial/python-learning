a9924727549a7de18768ae326e40d0a4e4e182fd
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-12 23:57:28 +0530

    Add practice question 2

---
+++ CHAPTER-5/PQ2.py
@@ -0,0 +1,10 @@
+nums = [1, 4, 36, 9, 16, 25, 36, 49, 64, 81, 100, 36]
+x = 25
+idx = 0
+
+for val in nums:
+    if(val == x):
+        print("x ffound at index: ", idx)
+    else:
+        print("funding...")
+    idx += 1
