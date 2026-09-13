b975cb7dc70049708a825f36055856e955efc96b
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-13 23:51:07 +0530

    Add Python function program

---
+++ CHAPTER-6/function.py
@@ -0,0 +1,15 @@
+# function defination
+def calc_sum(a, b): #parameters
+    return a + b
+
+# function call
+sum = calc_sum(4, 6) #argument
+# print(sum)
+
+def printHello():
+    print('hello')
+
+
+
+printHello()
+printHello()
