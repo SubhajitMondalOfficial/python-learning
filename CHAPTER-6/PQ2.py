64376928c3f2b811c3c7c2b4df4caba5168caf31
Subhajit Mondal <subhajitmondal.dev@gmail.com>
2026-09-13 23:52:08 +0530

    Add practice question 2

---
+++ CHAPTER-6/PQ2.py
@@ -0,0 +1,19 @@
+def factorialOfNum(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)
+    return fact
+
+factorialOfNum(5)
+
+
+#USD to INR
+
+def CurrencyConverter(amount):
+    convert = amount * 94.8
+    print(convert)
+    return convert
+
+
+CurrencyConverter(5)
